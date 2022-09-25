df66cccecb98a28c1cdaf700f648c0894d4e8a61
nataliakif <nataliakif@gmail.com>
2022-09-25 20:07:33 +0300

    add styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,25 @@
-import Header from "./Header/Header";
+import Header from './Header/Header';
+import DoingFineModal from './modals/DoingFineModal/DoingFineModal';
+import { useState } from 'react';
 
 export const App = () => {
+  const [doingFineModal, setDoingFineModal] = useState(false);
+
+  const closeDoingFineModal = async () => {
+    setDoingFineModal(false);
+  };
+  const onClick = async values => {
+    setDoingFineModal(true);
+  };
   return (
     <>
-      <Header/>
+      <Header />
+      <button type="button" onClick={onClick}>
+        Modal
+      </button>
+      {doingFineModal && (
+        <DoingFineModal open={DoingFineModal} onClose={closeDoingFineModal} />
+      )}
     </>
   );
 };
