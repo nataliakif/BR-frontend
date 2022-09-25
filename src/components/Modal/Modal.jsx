5a85bf6dda4f1aea2e646538a78071cc56e6b6c5
nataliakif <nataliakif@gmail.com>
2022-09-25 18:44:09 +0300

    /add modal

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+import styles from './Modal.module.css';
+
+const ModalWrapper = ({ open, onClose, children }) => {
+  const modalRef = useRef(document.getElementById('modal-root'));
+  useEffect(() => {
+    const handleClose = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleClose);
+    return () => {
+      window.removeEventListener('keydown', handleClose);
+    };
+  }, [onClose]);
+
+  const handleCloseClick = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
+  return (
+    open &&
+    createPortal(
+      <div className={styles.overlay} onClick={handleCloseClick}>
+        <div className={styles.modal}>{children}</div>
+      </div>,
+      modalRef.current
+    )
+  );
+};
+
+ModalWrapper.propTypes = {
+  size: PropTypes.oneOf(['small', 'large']),
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default ModalWrapper;
