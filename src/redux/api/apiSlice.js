2960210ab1df5025534adae96b8f40953cf2e0ef
@s_zhsn <s.zhosan@gmail.com>
2022-09-25 11:29:31 +0300

    created booksApiSlice

---
+++ src/redux/api/apiSlice.js
@@ -0,0 +1,42 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+// import { logOut, setError } from '../auth/authSlice';
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'https://localhost:3001/api',
+
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token;
+    if (token) {
+      headers.set('Authorization', `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithErrorHandler = async (args, api, extraOptions) => {
+  // api.dispatch(setError(null));
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (!result?.meta?.response?.ok) {
+    //console.log('Ошибочка');
+    let errorMessage = '';
+    switch (result.meta.response.status) {
+      case 400:
+        errorMessage = 'Email or password is incorrect';
+        break;
+      case 401:
+        errorMessage = 'Email or password is incorrect';
+        break;
+      default:
+        errorMessage = 'Something went wrong! Try again!';
+    }
+    // api.dispatch(logOut());
+    //api.dispatch(setError(errorMessage));
+  }
+  return result;
+};
+
+export const apiSlice = createApi({
+  baseQuery: baseQueryWithErrorHandler,
+  endpoints: builder => ({}),
+});
---
+++ src/redux/books/booksApi.js
@@ -0,0 +1,64 @@
+import { apiSlice } from '../api/apiSlice';
+
+export const contactsApi = apiSlice.injectEndpoints({
+  endpoints: builder => ({
+    fetchBooks: builder.query({
+      query: () => '/books',
+      providesTags: ['Books'],
+    }),
+    createBook: builder.mutation({
+      query: ({ title, author, publicationDate, pageAmount }) => ({
+        url: '/books',
+        method: 'POST',
+        body: {
+          title,
+          author,
+          publicationDate,
+          pageAmount,
+        },
+      }),
+      invalidatesTags: ['Books'],
+    }),
+    deleteBook: builder.mutation({
+      query: bookId => ({
+        url: `/books/${bookId}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Books'],
+    }),
+    editBook: builder.mutation({
+      query: ({
+        bookId,
+        title,
+        author,
+        publicationDate,
+        pageAmount,
+        status,
+        resume,
+        rating,
+        readStatistics,
+      }) => ({
+        url: `/books/${bookId}`,
+        method: 'PATCH',
+        body: {
+          title,
+          author,
+          publicationDate,
+          pageAmount,
+          status,
+          resume,
+          rating,
+          readStatistics,
+        },
+      }),
+      invalidatesTags: ['Books'],
+    }),
+  }),
+});
+
+export const {
+  useFetchBooksQuery,
+  useCreateBookMutation,
+  useDeleteBookMutation,
+  useEditBookMutation,
+} = contactsApi;
