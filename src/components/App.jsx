4752946109677098d11ce5b8c5997f9ec415b3ec
nataliakif <nataliakif@gmail.com>
2022-09-26 12:05:36 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,30 @@
-import Header from './Header/Header';
+import { Routes, Route } from 'react-router-dom';
+import { useEffect, lazy, Suspense } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-export const App = () => {
+const LoginView = lazy(() => import('../views/LoginView'));
+const RegisterView = lazy(() => import('../views/RegisterView'));
+const Header = lazy(() => import('../components/Header/Header'));
+const HomeView = lazy(() => import('../views/HomeView'));
+const LibraryView = lazy(() => import('../views/LibraryView'));
+const TrainingView = lazy(() => import('../views/TrainingView'));
+const NotFoundView = lazy(() => import('../views/NotFoundView'));
+
+function App() {
+  const dispatch = useDispatch();
+  const isRefreshCurrentUser = useSelector(authSelectors.isRefreshUserData);
+  useEffect(() => {
+    dispatch(authOperations.refreshCurrentUser());
+  }, [dispatch]);
   return (
     <>
-      <Header />
+      {!isRefreshCurrentUser && (
+        <Suspense fallback={<p>Loading...</p>}>
+          <Header />
+        </Suspense>
+      )}
     </>
   );
-};
+}
+
+export default App;
