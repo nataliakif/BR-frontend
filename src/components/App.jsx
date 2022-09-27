297d445c88023ef82d35dc0fbb1585dd32876bc2
Anna Tsherkavska <tsherkavskaanna@MBP-di-Anna.home-life.hub>
2022-09-27 21:24:26 +0200

    task 15

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,8 @@
 import Header from "./Header/Header";
-import TrainingWrapper from "./Training/TrainingWrapper";
 export const App = () => {
   return (
     <>
       <Header/>
-      <TrainingWrapper/>
     </>
   );
 };
