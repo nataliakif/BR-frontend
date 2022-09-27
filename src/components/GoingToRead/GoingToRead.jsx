39dafc2b8ebd8cf291bc6848b31a82c435731850
HordiienkoOlha <olhahordiienkomail@gmail.com>
2022-09-27 14:33:40 +0300

    add going to read mobile

--- src/components/GoingToRead/GoingToRead.jsx
+++ src/components/GoingToRead/GoingToRead.jsx
@@ -1,65 +1,21 @@
-import books from '../../dataFiles/book.json';
-import sprite from '../../images/sprite.svg';
-// import EllipsisText from 'react-ellipsis-text';
-// import { useState } from 'react';
+import GoingToReadMobile from './mobile/GoingToReadMobile';
+import GoingToReadMain from './GoingToReadMain/GoingToReadMain';
+import useIsMobile from "../../helpers/useIsMobile";
 
-import s from './GoingToRead.module.css';
-// import {
-//   createColumnHelper,
-//   flexRender,
-//   getCoreRowModel,
-//   useReactTable,
-// } from '@tanstack/react-table';
 // import s from "./GoingToRead.module.css"
 
-// const readingNowBooks = books.filter(book => book.status === 'Reading now');
-// console.log(readingNowBooks);
-
-const readingNowBooks = books.filter(book => {
-  const readPages = +book.read;
-  const pagesOfBook = +book.pages;
-
-  return readPages < pagesOfBook && readPages >= 1;
-});
-
-function GoingToRead() {
+const GoingToRead = () => {
+  const isMobile = useIsMobile();
+    
+    // if (isMobile) {
+    //     console.log("isMobile")
+    // }
   return (
     <>
-      <ul>
-        {readingNowBooks.map(({ id, title, author, year, pages }) => {
-          return (
-            <li className={s.cardBook} key={id}>
-              <div className={s.cardTitleBook}>
-                <div>
-                  <svg className="icon" width="22" height="17">
-                    <use href={sprite + '#icon-open-book'} />{' '}
-                  </svg>
-                </div>
-                <p className={s.cardTitle}>{title}</p></div>
-                <div className={s.cardTableBook}>
-                <table>
-                  <tbody>
-                    <tr>
-                      <th>Author</th>
-                      <td>{author}</td>
-                    </tr>
-                    <tr>
-                      <th>Year</th>
-                      <td>{year}</td>
-                    </tr>
-                    <tr>
-                      <th>Pages</th>
-                      <td>{pages}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+      
+      { isMobile ? <GoingToReadMobile /> : <GoingToReadMain/>}
     </>
   );
-}
+};
 
 export default GoingToRead;
