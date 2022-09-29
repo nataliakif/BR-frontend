ae6f3b452a5208b09801d6de54d79658b9d252fc
Nikita Osadchyi <nikitaosadchyi7@gmail.com>
2022-09-29 15:59:10 +0300

    feature/Header

--- src/components/Header/Logout/Logout.jsx
+++ src/components/Header/Logout/Logout.jsx
@@ -1,9 +1,35 @@
-import s from './Logout.module.css'
+import { useDispatch } from "react-redux";
+import { logOut } from '../../../redux/authUser/authUserSlice';
+import LogoutModal from "components/modals/LogoutModal/LogoutModal";
+import useToggle from "services/hooks/useToggle";
+
+import s from './Logout.module.css';
 
 const Logout = () => {
+    const dispatch = useDispatch();
+    // const [openModal, toggleModal] = useToggle();
+
+    const logoutFunc = () => {
+        dispatch(logOut());
+    }
     return (
         <>
-            <button className={s.logoutBtn}>Logout</button>
+            <button
+                type='button'
+                className={s.logoutBtn}
+                onClick={() => {
+                    logoutFunc();
+                }}
+            >
+                Logout
+            </button>
+            {/* {openModal && (
+                <LogoutModal
+                    open={openModal}
+                    onClose={toggleModal()}
+                    logoutFunc={logoutFunc}
+                />
+            )} */}
         </>
     )
 };
