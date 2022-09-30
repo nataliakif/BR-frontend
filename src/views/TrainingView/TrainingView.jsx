dd39cd75a7499b5d256008d7c4b9bf410ade8026
@s_zhsn <s.zhosan@gmail.com>
2022-10-01 00:52:14 +0300

    started this task

--- src/views/TrainingView/TrainingView.jsx
+++ src/views/TrainingView/TrainingView.jsx
@@ -2,15 +2,22 @@ import Container from 'components/Container';
 import MyGoals from 'components/MyGoals';
 import TrainingForm from 'components/Training/TrainingForm/TrainingForm';
 import s from './TrainingVieew.module.css';
+import { useState } from 'react';
 
 const TrainingView = () => {
+  const [startDate, setStartDate] = useState(null);
+  const [finishDate, setFinishDate] = useState(null);
+
   return (
     <>
       <Container>
         <div className={s.training}>
           <MyGoals />
           <div className={s.wrapperContainer}>
-            <TrainingForm />
+            <TrainingForm
+              onStartDateChange={setStartDate}
+              onFinishDateChange={setFinishDate}
+            />
           </div>
         </div>
       </Container>
