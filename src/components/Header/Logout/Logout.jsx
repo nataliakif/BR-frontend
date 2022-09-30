63f03de9309d32ee0298d43a7a271a76ee4114df
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-09-30 11:18:36 +0300

    s

--- src/components/Header/Logout/Logout.jsx
+++ src/components/Header/Logout/Logout.jsx
@@ -1,37 +1,39 @@
-import { useDispatch } from "react-redux";
+import { useDispatch } from 'react-redux';
 import { logOut } from '../../../redux/authUser/authUserSlice';
-import LogoutModal from "components/modals/LogoutModal/LogoutModal";
-import useToggle from "services/hooks/useToggle";
+import LogoutModal from 'components/modals/LogoutModal/LogoutModal';
+import useToggle from 'services/hooks/useToggle';
+import { Trans, useTranslation } from 'react-i18next';
 
 import s from './Logout.module.css';
 
 const Logout = () => {
-    const dispatch = useDispatch();
-    // const [openModal, toggleModal] = useToggle();
+  const dispatch = useDispatch();
+  const { t, i18n } = useTranslation();
+  // const [openModal, toggleModal] = useToggle();
 
-    const logoutFunc = () => {
-        dispatch(logOut());
-    }
-    return (
-        <>
-            <button
-                type='button'
-                className={s.logoutBtn}
-                onClick={() => {
-                    logoutFunc();
-                }}
-            >
-                Logout
-            </button>
-            {/* {openModal && (
+  const logoutFunc = () => {
+    dispatch(logOut());
+  };
+  return (
+    <>
+      <button
+        type="button"
+        className={s.logoutBtn}
+        onClick={() => {
+          logoutFunc();
+        }}
+      >
+        {t('description.part2')}
+      </button>
+      {/* {openModal && (
                 <LogoutModal
                     open={openModal}
                     onClose={toggleModal()}
                     logoutFunc={logoutFunc}
                 />
             )} */}
-        </>
-    )
+    </>
+  );
 };
 
 export default Logout;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { store, persistor } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
+import './services/i18n,js';
 import './index.css';
 import './styles/variables.css';
 
@@ -13,7 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter basename="/BR-frontend">
-          <App />
+          <Suspense fallback={<div>Loading...</div>}>
+            <App />
+          </Suspense>
         </BrowserRouter>
       </PersistGate>
     </Provider>
