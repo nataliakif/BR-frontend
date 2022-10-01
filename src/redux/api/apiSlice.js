ee9d623da67bde754d231ee3d71be6591ceaeecc
@s_zhsn <s.zhosan@gmail.com>
2022-10-01 23:21:18 +0300

    in process

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -2,8 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setError } from '../authUser/authUserSlice';
 
 const baseQuery = fetchBaseQuery({
-  //baseUrl: 'https://br-backend.herokuapp.com/',
-  baseUrl: 'http://localhost:3001/',
+  baseUrl: 'https://br-backend.herokuapp.com/',
 
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
