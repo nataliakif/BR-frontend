1c6b60257e26971ec5bb74e4d632bc095e91165b
nataliakif <nataliakif@gmail.com>
2022-10-06 21:44:34 +0300

    basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/BR-frontend">
+        <BrowserRouter basename="/">
           <Suspense fallback={<div>Loading...</div>}>
             <App />
           </Suspense>
